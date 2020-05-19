3b1e121534e71676a0c0dcf3562b3a72f4b75235
Donald Wu <yeukfei02@gmail.com>
2020-05-20 00:16:51 +0800

    feature/create-first-lib-#1: fix getWeather func

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 export async function getWeather(cityName: string) {
   let result = null;
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=089505f846c3f61791a473b77e84f8ed`,
-  );
-  if (response) {
-    result = response.json();
+  if (cityName.match(/^[a-zA-Z,\s]+$/g)) {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=089505f846c3f61791a473b77e84f8ed`,
+    );
+    if (response) {
+      result = response.json();
+    }
+  } else {
+    result = {
+      message: "please enter correct city name, state and country code",
+    };
   }
 
   return result;
 }
 
-console.log(await getWeather("!@#!@#!@#"));
+// const weather = await getWeather("hong kong");
+// console.log(weather);
